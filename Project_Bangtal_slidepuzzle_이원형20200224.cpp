273680c81ac1a84a501da4ba09cea1d75591325a
Brian <Brian@DESKTOP-LTAFP54>
2020-04-05 19:13:20 +0900

    새사진이용해서 타이머빼고완성

--- Project_Bangtal_slidepuzzle_이원형20200224.cpp
+++ Project_Bangtal_slidepuzzle_이원형20200224.cpp
@@ -18,13 +18,19 @@
     아래 : 1번, (0, +1)
     왼쪽 : 2번, (-1, 0)
     위 : 3번, (0, -1)
+
+    Y 좌표는 실제 이미지 좌표와 방향이 다름.
 */
 
 #include <Bangtal.h>
-#include <string>
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
 #pragma comment (lib, "Bangtal.lib")
+using namespace std;
 
 SceneID scene1;
+ObjectID startButton;
 ObjectID piece[16];
 
 // arr : 4 X 4 퍼즐의 현재상태를 저장.
@@ -34,16 +40,41 @@ int currentX[16], currentY[16];
 int dx[4] = { 1, 0, -1, 0 }, dy[4] = { 0, 1, 0, -1 };
 
 // 이미지 파일의 위치
-const char* imgFileLocate[16] = { "Images\\1.png", "Images\\2.png", "Images\\3.png", "Images\\4.png", "Images\\5.png", "Images\\6.png",
-        "Images\\7.png", "Images\\8.png" , "Images\\9.png" , "Images\\10.png" , "Images\\11.png" , "Images\\12.png",
-        "Images\\13.png" , "Images\\14.png" , "Images\\15.png", "Images\\16.png" };
+const char* imgFileLocate[16] = { "Images\\in1.jpg", "Images\\in2.jpg", "Images\\in3.jpg", "Images\\in4.jpg", "Images\\in5.jpg", "Images\\in6.jpg",
+        "Images\\in7.jpg", "Images\\in8.jpg" , "Images\\in9.jpg" , "Images\\in10.jpg" , "Images\\in11.jpg" , "Images\\in12.jpg",
+        "Images\\in13.jpg" , "Images\\in14.jpg" , "Images\\in15.jpg", "Images\\in16.jpg" };
 
 // hideX,Y,Num.  현재 hide된 piece의 X,Y좌표와 몇번째인지
 int hX = 0, hY = 0, hNum=0;
 
+bool correct[16];
+
+bool playing = false;
+bool finish = false;
 
 
+// for testing bug
+void test() {
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++) {
+            printf("%d ", arr[j][i]);
+        }
+        printf("\n");
+    }
+}
 
+int min(int a, int b) {
+    return a > b ? b : a;
+}
+
+int myRand(int n) {
+    int k = rand() % n;
+    if (n > 0) {
+        return k;
+    }
+    return 0;
+
+}
 
 // num 번째 조각에 배열에서의 원래 위치의 X좌표
 int arrayX(int num) {
@@ -55,18 +86,21 @@ int arrayY(int num) {
 }
 // 배열에서 x,y 좌표는 원래 number번째 조각.
 int number(int x, int y) {
-    return 4 * y + x;
+    return (4 * y + x);
 }
-
+// 반댓방향 번호
+int oppositeDirection(int d) {
+    return ((d + 2) % 4);
+}
 
 // (x,y)좌표로부터 이미지상 X좌표.
 // 사실 여기서 y는 안쓰이는 파라미터지만, coolX2 이런식으로 명명하면 훨씬 헷갈리니까
 // 좌표값을 받는다는 형식이란걸 일단 이렇게 표기합시다.
 int coolX(int x, int y) {
-    return 305 + 150 * x;
+    return 340 + 150 * x;
 }
 int coolY(int x, int y) {
-    return 460 - 150 * y;
+    return 510 - 150 * y;
 }
 // num 번쨰 조각의 원래 위치의 이미지상 X좌표
 int coolX(int num) {
@@ -89,12 +123,18 @@ int findNum(ObjectID object) {
     return -1; // piece가 아닌 object가 들어오면 -1.
 }
 
+// x+dx[direction], y+dy가 배열 안인지 확인
+bool checkIn(int x, int y, int direction) {
+    int tx = x + dx[direction], ty = y + dy[direction];
+    return (tx > -1 && tx < 4 && ty > -1 && ty < 4);
+}
+
 // arr[4][4]를 초기화 + initObject가 true면 piece 위치도 초기화
 void initializeArr(bool initObject) {
     int num = 0;
     for (int i = 0; i < 4; i++) {
         for (int j = 0; j < 4; j++) {
-            arr[i][j] = num;
+            arr[j][i] = num;
             if (initObject) {
                 locateObject(piece[num], scene1, coolX(num), coolY(num));
             }
@@ -107,6 +147,30 @@ void initializeCurrentXY() {
     for (int i = 0; i < 16; i++) {
         currentX[i] = arrayX(i);
         currentY[i] = arrayY(i);
+    }
+}
+// correct 초기화
+void initializeCorrect() {
+    for (int i = 0; i < 16; i++) {
+        correct[i] = true;
+    }
+}
+bool checkFin() {
+    for (int i = 0; i < 16; i++) {
+        if (!correct[i]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+void updateCorrect(int x, int y) {
+    int num = number(x, y);
+    if (arr[x][y] == num) {
+        correct[num] = true;
+    }
+    else {
+        correct[num] = false;
     }
 }
 
@@ -124,41 +188,113 @@ int checkHPieceDirection(int x, int y) {
 }
 
 
+void shuffle() {
+    
+    hNum = myRand(16);
+    hX = arrayX(hNum);
+    hY = arrayY(hNum);
+    hideObject(piece[hNum]);
+   
+
+    // 128번 섞기
+    for (int j = 0; j < 128; j++) {
+        // directionIn[i] : 갈수있는 방향 중 i번째 방향
+        int directionIn[4] = { 0, 0, 0, 0 };
+        int n = 0, r = -1;
+
+
+        // 갈수있는 방향 갯수 n 구하기 + 갈수있는 방향들에 번호부여
+        for (int i = 0; i < 4; i++) {
+            if (checkIn(hX, hY, i)) {
+                n++;
+                directionIn[n - 1] = i;
+            }
+        }
+
+        r = myRand(n);
+        //directionIn[r] 번째 방향으로 갈거임 ( = 그 방향 조각과 맞바꿀거임)
+
+        int dNum = directionIn[r];
+
+        int sx = hX + dx[dNum], sy = hY + dy[dNum];
+
+        if (sx < 4) {
+            int sNum = arr[sx][sy];
+
+            int tx = hX, ty = hY;
+
+            hX = sx, hY = sy;
+            currentX[hNum] = sx, currentY[hNum] = sy;
+            arr[sx][sy] = hNum;
+
+            currentX[sNum] = tx, currentY[sNum] = ty;
+            arr[tx][ty] = sNum;
+
+            locateObject(piece[hNum], scene1, coolX(sx, 0), coolY(0, sy));
+            locateObject(piece[sNum], scene1, coolX(tx, 0), coolY(0, ty));
+
+            updateCorrect(sx, sy);
+            updateCorrect(tx, ty);
+        }
+
+    }
+}
 
 
 void mouseCallback(ObjectID object, int x, int y, MouseAction) {
-    
-    int num = findNum(object);
-    // piece가 아닌걸 클릭하면 -1 반환했으니, piece중 하나 클릭한 경우만
-    if (num >= 0) {
-        int cx = currentX[num], cy = currentY[num];
-        int direction = checkHPieceDirection(cx, cy);
-        // 만약 인접해있다면, 자리 맞바꾸기
-        if (direction > -1) {
-            int tx = hX, ty = hY;
+    if (object == startButton) {
+        hideObject(startButton);
+        playing = true;
+        shuffle();
+    }
+    else if(playing){
+        // num : 클릭한 조각 번호
+        int num = findNum(object);
 
-            //hPiece 옮기기
-            hX = cx, hY = cy;
-            currentX[hNum] = cx, currentY[hNum] = cy;
-            arr[cx][cy] = hNum;
+        // piece가 아닌걸 클릭하면 -1 반환했으니, piece중 하나 클릭한 경우만
+        if (num >= 0) {
+            int cx = currentX[num], cy = currentY[num];
+            int direction = checkHPieceDirection(cx, cy);
+            // 만약 인접해있다면, 자리 맞바꾸기
+            if (direction > -1) {
 
-            //클릭한 piece 옮기기
-            currentX[num] = tx, currentY[num] = ty;
-            arr[tx][ty] = num;
+                int tx = hX, ty = hY;
 
-            locateObject(piece[hNum], scene1, coolX(num), coolY(num));
-            locateObject(piece[num], scene1, coolX(cx + dx[direction], 0), coolY(0, cy + dy[direction]));
-            
+                //hPiece 옮기기
+                hX = cx, hY = cy;
+                currentX[hNum] = cx, currentY[hNum] = cy;
+                arr[cx][cy] = hNum;
+
+                //클릭한 piece 옮기기
+                currentX[num] = tx, currentY[num] = ty;
+                arr[tx][ty] = num;
+
+                //이미지 갱신
+                locateObject(piece[hNum], scene1, coolX(cx, 0), coolY(0, cy));
+                locateObject(piece[num], scene1, coolX(tx, 0), coolY(0, ty));
+
+                updateCorrect(cx, cy);
+                updateCorrect(tx, ty);
+
+                finish = checkFin();
+                if (finish) {
+                    playing = false;
+                    showObject(startButton);
+                    showObject(piece[hNum]);
+                }
+            }
         }
+        
     }
-    }
+}
+
 
 int main()
 {
     setMouseCallback(mouseCallback);
 
     
-    scene1 = createScene("틀린그림찾기", "Images\\TEST.png");
+    scene1 = createScene("틀린그림찾기", "Images\\배경.jpg");
 
     // 조각 생성
     for (int i = 0; i < 16; i++) {
@@ -167,13 +303,25 @@ int main()
         showObject(piece[i]);
     }
 
+    startButton = createObject("시작버튼", "Images\\start.png");
+    locateObject(startButton, scene1, 590, 80);
+    showObject(startButton);
+
     initializeArr(false);
     initializeCurrentXY();
-
-
-    hX = 0, hY = 0, hNum = 0; // TODO 아직 셔플기능 안넣었으므로 임의지정
+    initializeCorrect();
+    
+    /*
+    srand((unsigned int)time(NULL));
+    hNum = rand() % 15;
+    hX = arrayX(hNum);
+    hY = arrayY(hNum);
+     
     hideObject(piece[hNum]);
-
+    */
+    //shuffle();
+    //test();
+    
     
     startGame(scene1);
     
